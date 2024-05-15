916cdaf3db292b543a79db80636b7ad736661bb6
José Lucas do Nascimento Camelo <161843482+jlcamelo2350@users.noreply.github.com>
2024-05-15 09:51:17 -0300

    Update and rename Regressão-Inov+Freedom x PIB per(1).r to Regressão-Inov+Freedom x PIB per.r

--- Análise da relação renda-inovação e liberdade econômica/Regressão-Inov+Freedom x PIB per.r
+++ Análise da relação renda-inovação e liberdade econômica/Regressão-Inov+Freedom x PIB per.r
@@ -1,5 +1,9 @@
 install.packages("readxl")
 library(readxl)
+install.packages("ggplot2")
+library(ggplot2)
+
+#Carrego os dados no R
 caminho <- "C:/Users/Vitória/Downloads/reg/countries.xlsx" ##localização do arquivo
 dados <- read_excel(caminho)
 
@@ -10,25 +14,30 @@ dados$xL <- as.numeric(dados$xL)
 dados$Y <- dados$xI + dados$xL + rnorm(nrow(dados))
 
 ##Crio dois modelos de regressão simples:
+#Dependência média da renda em relação a inovação:
 regI <- lm(Y~xI, data=dados)
+
+#Dependência média da renda em relação a liberdade econômica:
 regL <- lm(Y~xL, data=dados)
 
-##Crio um modelo de regressão múltiplo:
+##Crio um modelo de regressão múltiplo: renda e sua dependência em relação a inovação e liberdade econômica
 reg <- lm(Y~xI+xL, data=dados)
 
 ##Agora desenho o gráfico de cada modelo:
 
 ## 1- Modelos simples:
+
 ##1a) Usando o geom_smooth. Sintaxe: geom-smooth(method = "", formula = , se = FALSE, color = ""):
 
+#Gráfico de regressão entre inovação e renda:
 ggplot(dados, aes(x=xI, y=Y))+
 geom_point()+
 geom_smooth(method = "lm", formula = y~x, se = FALSE, color = "red")+
 labs(x = "Innovation", y="Pib per", title = "Regress. Linear: Inovação x Pib per Capita")
-##Resultado: 
 
 ##1b) Usando o geom_line:
 
+#Gráfico de regressão entre liberdade econômica e renda:
 dados$pred <- predict(regL) ##estimo os valores previstos para Yi no modelo Y~xL
 ggplot(dados,aes(x=xL, y=Y))+
 geom_point()+
@@ -36,8 +45,8 @@ geom_line(aes(y=pred), color="blue")+
 labs(x="economic freedom", y="PIB per", title="Regress. Linear: Liberdade x PIB")
 
 
-
 ##2- Modelo Múltiplo:
+#Gráfico de regressão entre liberdade econômica+inovação e renda:
 ggplot(dados, aes(x = Y, y = predict(reg)))+
 geom_point() +
 geom_smooth(method = "lm", formula = y~x, se = FALSE, color = "navy") +
