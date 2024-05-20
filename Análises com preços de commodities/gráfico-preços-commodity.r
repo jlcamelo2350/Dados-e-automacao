82f07b57728c779078393168ccf1d8279f62118f
José Lucas do Nascimento Camelo <161843482+jlcamelo2350@users.noreply.github.com>
2024-05-20 13:27:49 -0300

    Update gráfico-preços-commodity.r

--- Análises com preços de commodities/gráfico-preços-commodity.r
+++ Análises com preços de commodities/gráfico-preços-commodity.r
@@ -1,13 +1,13 @@
 library(readxl)  # Carrega a biblioteca `readxl` para leitura de arquivos Excel
 library(ggplot2) # Carrega a biblioteca `ggplot2` para criação de gráficos
 
-# Define o caminho do arquivo Excel
-caminho <- 'C:/Users/Vitória/Downloads/commodities(1).xlsx'
+# Define o local onde está o arquivo Excel
+caminho <- 'C:/Users/Camelo/Downloads/commodities(1).xlsx'
 
 # Lê o conteúdo do arquivo Excel em um data frame
 dados <- read_excel(caminho)
 
-# Extrai os nomes das commodities (exceto a primeira coluna)
+# Extrai os nomes das commodities (exceto a primeira coluna (data))
 commodities <- colnames(dados)[-1]
 
 # Loop para criar gráficos de cada commodity
@@ -19,7 +19,7 @@ for(commodity in commodities) {
     # Adiciona uma camada de área com cor azul marinho
     + geom_area(fill = "navy") 
     
-    # Define os títulos e rótulos dos eixos
+    # Define os títulos e rótulos dos eixos: título e nome dos eixos
     + labs(title = paste("Preço da Commodity", commodity, "ao Longo do Tempo"),
            x = "Data", y = "Preço da Commodity") 
     
