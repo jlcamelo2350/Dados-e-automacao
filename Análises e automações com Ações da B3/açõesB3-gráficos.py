aa4e5eda3b36671ceeda5f41f1cbb0792e02a445
José Lucas do Nascimento Camelo <161843482+jlcamelo2350@users.noreply.github.com>
2024-05-30 14:29:38 -0300

    Update açõesB3-gráficos.py

--- Análises e automações com Ações da B3/açõesB3-gráficos.py
+++ Análises e automações com Ações da B3/açõesB3-gráficos.py
@@ -12,33 +12,43 @@ import random
 # URL do arquivo Excel no Google Drive (link de download direto)
 url = 'https://docs.google.com/spreadsheets/d/1fG-I1Sn3sv_vIFZAGl2C54zLvw-Xqmit/export?format=xlsx'
 
-# específico o local onde guardarei o arquivo
+# especifico o local onde guardarei o arquivo
 downloads_dir = os.path.join(os.path.expanduser('~'), 'Downloads')
 caminho = os.path.join(downloads_dir, 'portfólio_RV.xlsx')
 
-# Faz o download do arquivo
+# Fazer o download do arquivo
 gdown.download(url, caminho, quiet=False)
 
 # Carregar os dados do arquivo Excel
 dados = pd.read_excel(caminho, index_col=0, parse_dates=True)
 
+#Obtenho os nomes das colunas
 empresas = dados.columns
 
-# Plotar um gráfico de linha para cada coluna da planilha
+# Plotar um gráfico de linha e um outro de tendência para cada coluna da planilha 
 for empresa in empresas:
     plt.figure(figsize=(12, 6))
 
     # Desenho um gráfico de linhas para cada empresa
     plt.plot(dados.index, dados[empresa], label=empresa, color=np.random.choice(['red', 'blue', 'green']))
     
-    # Desenho uma linha de tendência para empresa
+    # Desenho uma linha de tendência para a empresa
     coef = np.polyfit(range(len(dados)), dados[empresa], 1)
     tendencia = np.polyval(coef, range(len(dados)))
     plt.plot(dados.index, tendencia, linestyle='--', color='black')
 
+    #Nomeio os eixos
     plt.xlabel('Data')
     plt.ylabel('Preço da Ação')
+
+    #Entitulo o gráfico
     plt.title(f'Preço da Ação {empresa} ao Longo do Tempo')
+
+    #Mostro legendas
     plt.legend()
+
+    #Mostro as grades no gráfico
     plt.grid(True)
+
+    #Mostro os gráficos
     plt.show()
